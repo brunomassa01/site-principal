495b18919d35c1d58d272c0ef77f3d81925afcb4
Bruno Massa <37840539+brunomassa01@users.noreply.github.com>
2026-05-22 17:29:06 +0000

    fix: remove sitemap temporariamente (bug com i18n)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,7 +8,7 @@ export default defineConfig({
   server: { host: true },
   integrations: [
     tailwind(),
-    sitemap(),
+
   ],
   i18n: {
     defaultLocale: 'pt',
