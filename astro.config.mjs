fb3feb8ade09da526e7304ee138b8107d6e3765d
Bruno Massa <37840539+brunomassa01@users.noreply.github.com>
2026-05-22 15:57:54 +0000

    chore: host para Codespaces, lock file e env types

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://brunomassa.online',
+  server: { host: true },
   integrations: [
     tailwind(),
     sitemap(),
